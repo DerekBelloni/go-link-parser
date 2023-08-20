8918f1e58696b07037a5d93b921fc93b3a2a645d
Derek Belloni <dbelloni@compunet.biz>
2023-08-20 15:35:25 -0600

    refactored app, abstracted most the code out of main.go and put in a 'links' package

--- main.go
+++ main.go
@@ -5,93 +5,33 @@ import (
 	"log"
 	"os"
 	"strings"
-
-	"golang.org/x/net/html"
+	"github.com/derekbelloni/go-link-parser/pkg/links"
 )
 
-type Link struct {
-	Href string
-	Text string
-	// Attribute string
-}
-
 func main() {
-	fileName := "ex1.html"
+	fileName := "ex3.html"
 
 	text, err := readHtmlFromFile(fileName)
 	if err != nil {
 		log.Fatal("Error reading html file: ", err)
 	}
 
-	links := parseTokens(text)
+	r := strings.NewReader(text)
 
-	fmt.Println("parsed links: ", links)
-}
-
-func parseTokens(text string) []Link {
-	doc, err := html.Parse(strings.NewReader(text))
-	if err !=  nil {
-		log.Fatal("Error: ", err)
+	links, err := links.Parse(r)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	nodes := linkNodes(doc)
-	var links []Link
-
-	for _, n := range nodes{
-		links = append(links, buildLink(n))
-	}
-
-	return links
-}
-
-func linkNodes(n *html.Node) []*html.Node {
-	if n.Type == html.ElementNode && n.Data == "a" {
-		return []*html.Node{n}
-	}
-
-	var ret []*html.Node
-
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		ret = append(ret, linkNodes(c)...)
-	}
-
-	return ret
-}
-
-func buildLink(n *html.Node) Link {
-	var ret Link
-
-	for _, a := range n.Attr {
-		if a.Key == "href" {
-			ret.Href = a.Val
-		}
-	}
-	ret.Text = text(n)
-
-	return ret
-}
-
-func text(n *html.Node) string {
-	fmt.Println(n.Data)
-	if n.Type == html.TextNode {
-		return n.Data
-	}
-	if n.Type == html.ElementNode {
-		return ""
-	}
-	var ret string
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		ret += text(c)
-	}
-
-	return ret
+	fmt.Printf("%+v\n", links)
 }
 
 func readHtmlFromFile(fileName string) (string, error) {
-	htmlFile, err := os.ReadFile("ex2.html")
+	htmlFile, err := os.ReadFile(fileName)
 	if err != nil {
 		log.Fatal("Error: ", err)
 	}
 
 	return string(htmlFile), nil
 }
+
